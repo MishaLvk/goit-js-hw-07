dffaea418a41b30cfb62d7eb8159a024d933d75b
Misha Levko <lvk.misha@gmail.com>
2022-08-15 23:04:01 +0300

    finish_ex_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryMarcup = createGallery(galleryItems);
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarcup);
+// gallery.addEventListener("click", oneImageClick);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
