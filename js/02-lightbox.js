0629adf37398f22eac6cdf3e00dde571e0199e6b
Misha Levko <lvk.misha@gmail.com>
2022-08-16 08:52:50 +0300

    fixed_ex_2_li

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,9 +9,9 @@ gallery.insertAdjacentHTML("beforeend", galleryMarcup);
 function createGallery(galleryItems) {
   return galleryItems
     .map(({ original, preview, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
     })
     .join("");
 }
